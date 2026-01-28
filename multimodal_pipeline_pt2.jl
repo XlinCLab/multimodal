721cb61cbe0d7a0a80ddb20dd142316443770eba
Philip Georgis <philip.georgis@gmail.com>
2026-01-28 17:35:54 +0100

    Correct path issue causing double frames levels in outdir for video frame extraction

--- multimodal_pipeline_pt2.jl
+++ multimodal_pipeline_pt2.jl
@@ -1,11 +1,10 @@
 function multimodal_pipeline_pt2(args)
     outdir = abspath(args["outdir"])
     frames_csv = abspath(args["frames_csv"])
-    frames_outdir = joinpath(outdir, "frames")
     multimodal_yolo_path = abspath(args["multimodal_yolo_path"])
     multimodal_yolo_venv_python = joinpath(multimodal_yolo_path, ".venv", "bin", "python")
     frame_extraction_script = joinpath(multimodal_yolo_path, "efficient_frames_extracting.py")
-    frame_extraction_cmd = `$(multimodal_yolo_venv_python) $(frame_extraction_script) --input_csv $(frames_csv) --outdir $(frames_outdir)`
+    frame_extraction_cmd = `$(multimodal_yolo_venv_python) $(frame_extraction_script) --input_csv $(frames_csv) --outdir $(outdir)`
     run(frame_extraction_cmd)
     yolo_detect_docker_compose = joinpath(multimodal_yolo_path, "docker-compose.detect.yml")
     # Adjust file paths in docker compose file
